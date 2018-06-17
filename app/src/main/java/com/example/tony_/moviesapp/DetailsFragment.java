fb1668d878c616c9b91f47eff69129d3edecdc8e
sang <stran8089@gmail.com>
2018-06-17 13:08:41 -0400

    adding settings menu

--- app/src/main/java/com/example/tony_/moviesapp/DetailsFragment.java
+++ app/src/main/java/com/example/tony_/moviesapp/DetailsFragment.java
@@ -8,7 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -34,11 +37,21 @@ public class DetailsFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_details, container, false);
 
-        TextView titleTextView = view.findViewById(R.id.tv_details_name);
+        TextView titleTextView = view.findViewById(R.id.tv_title);
+        TextView dateTextView = view.findViewById(R.id.tv_details_date);
         TextView ratingTextView = view.findViewById(R.id.tv_details_rating);
+        TextView plotTextView = view.findViewById(R.id.tv_details_plot);
+        ImageView posterImageView = view.findViewById(R.id.iv_details_poster);
 
         titleTextView.setText(mMovie.getmTitle());
         ratingTextView.setText(mMovie.getmRating());
+        dateTextView.setText(mMovie.getmReleaseDate());
+        plotTextView.setText(mMovie.getmPlot());
+
+        Picasso.get()
+                .load(mMovie.getmPosterUrl())
+                .error(R.drawable.user_placeholder_error)
+                .into(posterImageView);
 
         return view;
     }
